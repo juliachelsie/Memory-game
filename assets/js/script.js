694621f6f1fa5625c81a3c06ce076d2d55653248
juliachelsie <>
2023-12-22 16:00:40 +0000

    Added function to match the cards in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,38 @@ let firstCard, secondCard;
     // Function to turn the cards //
 function turnCard(e) {
     let cardClicked = e.target;
-    if (cardClicked == firstCard) {
+    if (cardClicked != firstCard) {
         cardClicked.classList.add('turn');
         if (!firstCard) {
-            return (firstCard = cardClicked);
+            return(firstCard = cardClicked);
         }
-
-        secondCard = cardClicked;
-
-        let firstCardImg = firstCard.querySelector('.img').src, 
-            secondCardImg = secondCard.querySelector('.img').src;
-        cardsmatch(firstCardImg, secondCardImg);
     }
+
+    secondCard = cardClicked;
+
+    let firstCardImg = firstCard.querySelector('img1').src,
+        secondCardImg = secondCard.querySelector('img2').src;
+    cardsMatch(firstCardImg, secondCardImg);
+    
 }
 
+function cardsMatch(img1, img2) {
+    if (img1==img2) {
+        firstCard.removeEventListener('click', 'turnCard'),
+        secondCard.removeEventListener('click', 'turnCard');
+
+        firstCard = secondCard = '';
+        return;
+    }
+    
+    
+    setTimeout(() => {
+        firstCard.classList.remove('turn'),
+        secondCard.classList.remove('turn');
+
+        firstCard = secondCard = '';
+    }, 1000);
+}
 
 cards.forEach((card) => {
     card.addEventListener('click', turnCard);
